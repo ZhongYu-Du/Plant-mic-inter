cd768e9bd0dbc4c1a2b1b41200d00c4d35d2f77b
ShimmerQ <158442895+ShimmerQ@users.noreply.github.com>
2024-02-05 10:55:24 +0800

    Update Sloan.R

--- Sloan.R
+++ Sloan.R
@@ -4,35 +4,6 @@ library(minpack.lm)
 library(stats4)
 library(dplyr)
 
-############tools############
-sloan<-function(df){
-  spp<-df
-  N <- mean(apply(spp, 1, sum))
-  p.m <- apply(spp, 2, mean)
-  p.m <- p.m[p.m != 0]
-  p <- p.m/N
-  spp.bi <- 1*(spp>0)
-  freq <- apply(spp.bi, 2, mean)
-  freq <- freq[freq != 0]
-  C <- merge(p, freq, by=0)
-  C <- C[order(C[,2]),]
-  C <- as.data.frame(C)
-  C.0 <- C[!(apply(C, 1, function(y) any(y == 0))),]
-  p <- C.0[,2]
-  freq <- C.0[,3]
-  names(p) <- C.0[,1]
-  names(freq) <- C.0[,1]
-  d = 1/N
-  m.fit <- nlsLM(freq ~ pbeta(d, N*m*p, N*m*(1 -p), lower.tail=FALSE),start=list(m=0.1))
-  m.ci <- confint(m.fit, 'm', level=0.95)
-  freq.pred <- pbeta(d, N*coef(m.fit)*p, N*coef(m.fit)*(1 -p), lower.tail=FALSE)
-  pred.ci <- binconf(freq.pred*nrow(spp), nrow(spp), alpha=0.05, method="wilson", return.df=TRUE)
-  Rsqr <- 1 - (sum((freq - freq.pred)^2))/(sum((freq - mean(freq))^2))
-  out_sloan<-data.frame(p,freq,freq.pred,pred.ci[,2:3])
-  label<-paste("R^2 =",round(Rsqr,4),"\n","Nm =", round(coef(m.fit)*N,4) ,"\n","m =", round(coef(m.fit),4))
-  sloan_label<-list(out_sloan,label)
-  return(sloan_label)}
-
 ############SCB############
 data_SCB<- read.csv('SCB.csv', row.names = 1)
 data_SCB<-t(data_SCB)
